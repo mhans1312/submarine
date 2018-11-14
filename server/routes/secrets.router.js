9bef7128b5d61b79eee248db3eab3b37ebf7229a
Matt Hansen <hans1312@gmail.com>
2018-11-14 16:17:59 -0600

    commit

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    res.send(req.user);
+  });
 
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
